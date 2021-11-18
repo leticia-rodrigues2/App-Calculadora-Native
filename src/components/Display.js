8b1225d00f283e7980c88c2fbf5f23e941bea96b
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-11-18 17:57:44 -0300

    Componente Display aula 58

---
+++ src/components/Display.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { 
+  StyleSheet,
+  Text,
+  View
+ } from "react-native";
+
+const styles = StyleSheet.create({
+  display :{
+     flex: 1,
+     padding: 20,
+     justifyContent: 'center' ,
+     borderBottomColor:'#Pink',
+     alignItems: 'flex-end',
+  },
+
+  displayValue: {
+     fontSize:60,
+     color: '#black',
+  }
+})
+export default props =>
+<View style={styles.display}>
+  <Text style={styles.displayValue}
+  numberOfLines={1}> {props.value}
+  </Text>
+</View>
