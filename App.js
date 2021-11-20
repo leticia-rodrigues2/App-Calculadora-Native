dd8b2e7fe6ffc622f1f982f77d8ca266cea37548
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-11-20 19:54:24 -0300

    Finalizando projeto calculadora aula 62

--- App.js
+++ App.js
@@ -42,6 +42,27 @@ export default class App extends Component{
 
   setOperation = operation =>{
 
+    if(this.state.current === 0) {
+      this.setState({operation , current : 1 ,clearDisplay :true})
+    }else{
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try{
+        values [0]=
+        eval(`${values[0]} ${this.state.operation} ${values [1]}`)
+      } catch (e){
+        values[0]= this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState ({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation ,
+        current: equals ? 0 :1, 
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
 
@@ -67,7 +88,7 @@ export default class App extends Component{
         <Button label = '+' operation onClick ={this.setOperation}/>
         <Button label = '0' double onClick = {this.setOperation} />
         <Button label = '.' onClick = {this.addDigit}/>
-        <Button label = '=' operation onClick ={this.addDigit}/>
+        <Button label = '=' operation onClick ={this.setOperation}/>
         </View>
       </View>
 
